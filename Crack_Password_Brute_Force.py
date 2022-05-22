b5d68cb1747f395cfe39d3775f23362bf3ede308
David Aw <89636227+newbietofx@users.noreply.github.com>
2022-05-22 22:34:10 +0800

    Update Crack_Password_Brute_Force.py

--- Crack_Password_Brute_Force.py
+++ Crack_Password_Brute_Force.py
@@ -1,7 +1,7 @@
 import random
-import string
+import time
 
-#Courtesy of https://github.com/MustafaTheCoder
+#courtesy of https://github.com/MustafaTheCoder
 
 def split(word):
     return list(word)
@@ -10,12 +10,21 @@ keep_going = True
 guess_password = ""
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!$%&'()*+,-./:;<=>?@[\]^_`{|}~"
 
-password = input("Enter Password: ")
 
+password = input("Enter Password: ")
+# store starting time
+begin = time.time()
+  
+# program body starts
 while keep_going:
     guess_password = random.choices(split(chars), k=len(password))
     print("<============"+ str(guess_password) +"============>")
     
     if guess_password == list(password):
 	    print("Cracked Password: " + "".join(guess_password))
+	    time.sleep(1)
+	    # store end time
+	    end = time.time()
+	    # total time taken
+	    print(f"Total runtime of the program is {end - begin} s")
 	    keep_going = False
